ef3e1030124abfb49ae85ffef545bb11e33dd28a
Jeremy Lloyd Conlin <jlconlin@lanl.gov>
2019-06-27 16:37:16 +0200

    Working.

---
+++ src/Mim.hpp
@@ -0,0 +1,9 @@
+#ifndef MIM_HPP
+#define MIM_HPP
+
+namespace njoy {
+namespace mim{
+
+} // namespace mim
+} // namespace njoy
+#endif  // MIM_HPP
---
+++ src/test/Mim.test.cpp
@@ -0,0 +1,8 @@
+#define CATCH_CONFIG_MAIN
+
+#include "catch.hpp"
+
+#include "Mim.hpp"
+
+SCENARIO( "Testing Mim" ){
+}
